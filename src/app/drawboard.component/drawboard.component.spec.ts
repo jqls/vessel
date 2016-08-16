a89763c6df0e2f8ede8e98f841a1a2f8fe53291d
Qiu Wenkai <chlwqwk@163.com>
2016-08-16 10:18:32 +0800

    修复ISSUE #5

--- src/app/drawboard.component/drawboard.component.spec.ts
+++ src/app/drawboard.component/drawboard.component.spec.ts
@@ -5,12 +5,13 @@ import {DebugElement} from '@angular/core';
 import {addProviders, async, inject} from '@angular/core/testing';
 import {DrawboardComponent} from './drawboard.component';
 import {DrawboardStatusService} from "../drawboard-status.service";
+import {ParametersStatusService} from "../parameters-status.service";
 
 describe('Component: Drawboard', () => {
   let service = new DrawboardStatusService();
-
+  let parameterService = new ParametersStatusService();
   it('should create an instance', () => {
-    let component = new DrawboardComponent(service);
+    let component = new DrawboardComponent(service, parameterService);
     expect(component).toBeTruthy();
   });
 });
