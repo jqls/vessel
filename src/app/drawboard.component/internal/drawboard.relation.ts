dd7b82c5b99583180ce56eb79256c6fecd21a9e5
Qiu Wenkai <chlwqwk@163.com>
2016-08-17 15:29:21 +0800

    1.修改drawboard.element.ts中的addMenuTo方法，并对相应调用进行修改； 2.修改drawboard.element.ts以及drawboard.relation.ts的deleteElements方法的参数及实现 3.修改drawboard.element.ts中的deleteElements方法，实现删除关联连线

--- src/app/drawboard.component/internal/drawboard.relation.ts
+++ src/app/drawboard.component/internal/drawboard.relation.ts
@@ -8,99 +8,108 @@ import {Menu} from "./drawboard.menu";
 
 
 export class Relation {
-  from: DrawboardElement;
-  to: DrawboardElement;
-  path: any;
+    from:DrawboardElement;
+    to:DrawboardElement;
+    path:any;
 
-  menu: any;
+    menu:any;
 
-  initMenu(): void {
-    this.menu = new Menu();
-    this.menu.addItem("删除",this.deleteElements);
-    this.menu.addMenuTo(this.path.node());
-    console.log("relation-init");
+    initMenu():void {
+        this.menu = new Menu();
+        this.menu.addItem("删除", this.deleteElements);
+        this.menu.addMenuTo(this);
+        console.log("relation-init");
 
-    console.log(this.menu);
-  }
-
-  deleteElements(obj: any,self: any): void {
-    console.log("delete");
-    d3.select(obj).remove();
-    // console.log(self);
-    let menu = d3.select(self);
-    // console.log(menu);
-    menu.remove();
-  }
-
-  constructor(drawboard: DrawboardComponent, from: DrawboardElement, to: DrawboardElement) {
-    this.path = drawboard.relationLayer.append('path');
-    this.from = from;
-    this.to = to;
-    this.path.classed('path', true)
-      .style('marker-end', 'url(#mark-end-arrow)');
-    this.update();
-    this.initMenu();
-  }
-
-  getToPosition(): {x: number, y: number} {
-    var x3 = 0, y3 = 0;
-    let deltaY = this.to.cy - this.from.cy;
-    let deltaX = this.to.cx - this.from.cx;
-    // console.log("deltaX = "+deltaX+"deltaY = "+deltaY);
-    if (Math.abs(deltaX) < 1) {
-      return {
-        'x': this.to.cx + ELEMENT_WIDTH / 2,
-        'y': this.to.cy
-      };
+        // console.log(this.menu);
     }
 
-    let k0 = ELEMENT_HEIGHT / ELEMENT_WIDTH;
-    let k = -(deltaY / deltaX);
-    // console.log(k);
-    if (Math.abs(k) <= k0) {
-      if (deltaX >= 0) {
-        // console.log("-----1------");
-        x3 = this.to.cx;
-        y3 = this.to.cy + ELEMENT_HEIGHT / 2 + ELEMENT_WIDTH / 2 * k;
-      } else {
-        // console.log("-----2------");
-        x3 = this.to.cx + ELEMENT_WIDTH;
-        y3 = this.to.cy + ELEMENT_HEIGHT / 2 - ELEMENT_WIDTH / 2 * k;
-      }
-    } else {
-      if (deltaY >= 0) {
-        // console.log("-----3------");
-        x3 = this.to.cx + ELEMENT_WIDTH / 2 + ELEMENT_HEIGHT / 2 / k;
-        y3 = this.to.cy;
-      } else {
-        // console.log("-----4------");
-        x3 = this.to.cx + ELEMENT_WIDTH / 2 - ELEMENT_HEIGHT / 2 / k;
-        y3 = this.to.cy + ELEMENT_HEIGHT;
-      }
+    deleteElements(myself:Relation):void {
+        console.log("delete");
+        myself.path.remove();
+        // console.log(self);
+        let menu = d3.select(myself.menu.menu);
+        // console.log(menu);
+        menu.remove();
     }
 
-    return {
-      'x': x3,
-      'y': y3
-    };
-  }
+    deleteElement():void {
+        console.log("deleteElement");
+        this.path.remove();
+        // console.log(self);
+        let menu = d3.select(this.menu.menu);
+        // console.log(menu);
+        menu.remove();
+    }
 
-  getFromPosition(): {x: number, y: number} {
-    return {
-      'x': this.from.cx + ELEMENT_WIDTH / 2,
-      'y': this.from.cy + ELEMENT_HEIGHT / 2
-    };
-  }
+    constructor(drawboard:DrawboardComponent, from:DrawboardElement, to:DrawboardElement) {
+        this.path = drawboard.relationLayer.append('path');
+        this.from = from;
+        this.to = to;
+        this.path.classed('path', true)
+            .style('marker-end', 'url(#mark-end-arrow)');
+        this.update();
+        this.initMenu();
+    }
 
-  getDAttribute(): string {
-    let fromPosition = this.getFromPosition();
-    let from = 'M' + fromPosition.x + ',' + fromPosition.y;
-    let toPosition = this.getToPosition();
-    return from + 'L' + toPosition.x + ',' + toPosition.y;
-  }
+    getToPosition():{x:number, y:number} {
+        var x3 = 0, y3 = 0;
+        let deltaY = this.to.cy - this.from.cy;
+        let deltaX = this.to.cx - this.from.cx;
+        // console.log("deltaX = "+deltaX+"deltaY = "+deltaY);
+        if (Math.abs(deltaX) < 1) {
+            return {
+                'x': this.to.cx + ELEMENT_WIDTH / 2,
+                'y': this.to.cy
+            };
+        }
+
+        let k0 = ELEMENT_HEIGHT / ELEMENT_WIDTH;
+        let k = -(deltaY / deltaX);
+        // console.log(k);
+        if (Math.abs(k) <= k0) {
+            if (deltaX >= 0) {
+                // console.log("-----1------");
+                x3 = this.to.cx;
+                y3 = this.to.cy + ELEMENT_HEIGHT / 2 + ELEMENT_WIDTH / 2 * k;
+            } else {
+                // console.log("-----2------");
+                x3 = this.to.cx + ELEMENT_WIDTH;
+                y3 = this.to.cy + ELEMENT_HEIGHT / 2 - ELEMENT_WIDTH / 2 * k;
+            }
+        } else {
+            if (deltaY >= 0) {
+                // console.log("-----3------");
+                x3 = this.to.cx + ELEMENT_WIDTH / 2 + ELEMENT_HEIGHT / 2 / k;
+                y3 = this.to.cy;
+            } else {
+                // console.log("-----4------");
+                x3 = this.to.cx + ELEMENT_WIDTH / 2 - ELEMENT_HEIGHT / 2 / k;
+                y3 = this.to.cy + ELEMENT_HEIGHT;
+            }
+        }
+
+        return {
+            'x': x3,
+            'y': y3
+        };
+    }
+
+    getFromPosition():{x:number, y:number} {
+        return {
+            'x': this.from.cx + ELEMENT_WIDTH / 2,
+            'y': this.from.cy + ELEMENT_HEIGHT / 2
+        };
+    }
+
+    getDAttribute():string {
+        let fromPosition = this.getFromPosition();
+        let from = 'M' + fromPosition.x + ',' + fromPosition.y;
+        let toPosition = this.getToPosition();
+        return from + 'L' + toPosition.x + ',' + toPosition.y;
+    }
 
 
-  update() {
-    this.path.attr('d', this.getDAttribute());
-  }
+    update() {
+        this.path.attr('d', this.getDAttribute());
+    }
 }
