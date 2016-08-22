50f36520778ba6b259c6683d834d1f6bd2f48528
ttaanngg <tangweiqiang@hotmail.com>
2016-08-22 22:14:07 +0800

    当日结束

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,4 @@
-// // import {bootstrap} from "@angular/platform-browser-dynamic";
-//
-// import {enableProdMode} from "@angular/core";
-// import {AppComponent, environment} from "./app/";
-//
-// if (environment.production) {
-//   enableProdMode();
-// }
-//
-// // bootstrap(AppComponent);
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./app/app.module";
+
 platformBrowserDynamic().bootstrapModule(AppModule);
