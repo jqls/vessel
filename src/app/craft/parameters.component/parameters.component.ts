fa177b4dd5fc9074d95a59bac7e053fdfd5b69c6
Qiu Wenkai <chlwqwk@163.com>
2016-08-26 14:14:42 +0800

    对parameters添加默认显示，完善result.service

--- src/app/craft/parameters.component/parameters.component.ts
+++ src/app/craft/parameters.component/parameters.component.ts
@@ -5,39 +5,41 @@ import {DrawboardElement} from "../drawboard.component/internal/drawboard.elemen
 import {DataSourceNode, ProcessNode} from "../drawboard.component/internal/drawboard.node";
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-parameters',
-  templateUrl: 'parameters.component.html',
-  styleUrls: ['parameters.component.css'],
+    moduleId: module.id,
+    selector: 'app-parameters',
+    templateUrl: 'parameters.component.html',
+    styleUrls: ['parameters.component.css'],
 })
 export class ParametersComponent implements OnInit {
 
-  open: boolean;
-  openedNode: ProcessNode;
-  form: FormGroup;
+    open: boolean;
+    openedNode: ProcessNode;
+    form: FormGroup;
 
-  constructor(private parametersStatus: ParametersStatusService) {
-    this.open = false;
-    parametersStatus.bookService((node: ProcessNode): void=> {
-      this.open = (node != null);
-      this.openedNode = node;
-      this.form = parametersStatus.toFormGroup();
-    });
-  }
-  // setOpen(node: DrawboardElement): void{
-  //   if(node instanceof ProcessNode)
-  //     this.open = true;
-  //   else if (node instanceof DataSourceNode)
-  //     this.open = false;
-  // }
-  onSubmit() {
-    //todo: 更新参数
-    console.log("update parameters");
-    this.parametersStatus.updatePatameters(this.form);
-  }
+    constructor(private parametersStatus: ParametersStatusService) {
+        this.open = false;
+        parametersStatus.bookService((node: ProcessNode): void=> {
+            this.open = (node != null);
+            this.openedNode = node;
+            if (node != null)
+                this.form = parametersStatus.toFormGroup();
+        });
+    }
 
-  ngOnInit() {
+    // setOpen(node: DrawboardElement): void{
+    //   if(node instanceof ProcessNode)
+    //     this.open = true;
+    //   else if (node instanceof DataSourceNode)
+    //     this.open = false;
+    // }
+    onSubmit() {
+        //todo: 更新参数
+        console.log("update parameters");
+        this.parametersStatus.updatePatameters(this.form);
+    }
 
-  }
+    ngOnInit() {
+
+    }
 
 }
