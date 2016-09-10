0cd99aa9db2902b6546d28534b11c7c2ba7f0d5c
Qiu Wenkai <chlwqwk@163.com>
2016-09-10 10:15:55 +0800

    使用Http更新submit.service代码

--- src/app/craft/submit.service.ts
+++ src/app/craft/submit.service.ts
@@ -1,14 +1,27 @@
 import {Injectable} from "@angular/core";
+import {Headers, Http, RequestMethod} from "@angular/http";
 
 
 @Injectable()
 export class SubmitService {
 
-  constructor() {
+  private submitUrl = "http://10.5.0.224:8080/submit/";
+  constructor(private http:Http) {
   }
 
   submit(workflowJSON: string) {
     console.log(`submit: ${workflowJSON}`);
+    let headers = new Headers({
+      'Content-Type': 'application/json'});
+    console.log(JSON.stringify(workflowJSON));
+    return this.http
+        .post(this.submitUrl, JSON.stringify(workflowJSON), {headers: headers,method: RequestMethod.Post})
+        .toPromise()
+        .then(res => {console.log(res)})
+        .catch(this.handleError);
   }
-
+  private handleError(error: any) {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
 }
