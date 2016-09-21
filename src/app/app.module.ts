2efbb396961d7c8e38779e01ff718c14c9dead57
Qiu Wenkai <chlwqwk@163.com>
2016-09-22 00:42:59 +0800

    debug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,34 @@
  * Created by tang on 2016/8/18.
  */
 
+import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
-import {NgModule} from "@angular/core";
+import {HttpModule, JsonpModule, Response} from "@angular/http";
+import {MdButtonModule} from "@angular2-material/button";
+import {MdCardModule} from "@angular2-material/card";
+
+import {SelectComponent} from "ng2-select";
+
 import {AppComponent} from "./app.component";
 import {routing, appRoutingProvider} from "./app.routing";
 import {CraftComponent} from "./craft/craft.component";
-import {HttpModule, JsonpModule, Response} from "@angular/http";
+import { CraftStormComponent } from './craft/craft-storm.component';
 import {CraftMapreduceComponent} from "./craft/craft-mapreduce.component";
-import {DataAnalysisComponent} from "./data-analysis.unit/data-analysis.component";
-import {MdButtonModule} from "@angular2-material/button";
-import {MdCardModule} from "@angular2-material/card";
 import {NavbarComponent} from "./navbar.component/navbar.component";
 import {ToolboxComponent} from "./craft/toolbox.component/toolbox.component";
+import {ToolboxMapreduceComponent} from "./craft/toolbox.component/toolbox-mapreduce.component";
 import {DrawboardComponent} from "./craft/drawboard.component/drawboard.component";
 import {ParametersComponent} from "./craft/parameters.component/parameters.component";
 import {ResultComponent} from "./craft/result.component/result.component";
-import {ToolboxMapreduceComponent} from "./craft/toolbox.component/toolbox-mapreduce.component";
 import {InputsComponent} from "./craft/inputs.component/inputs.component";
+import {DataAnalysisComponent} from "./data-analysis.unit/data-analysis.component";
 import {DatabaseControlComponent} from "./data-analysis.unit/database-control/database-control.component";
 import {ResultPresentationComponent} from "./data-analysis.unit/result-presentation/result-presentation.component";
-import {SelectComponent} from "ng2-select";
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
     imports: [
@@ -35,12 +41,14 @@ import {SelectComponent} from "ng2-select";
         routing,
         MdButtonModule,
         MdCardModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService)
     ],
     declarations: [
         AppComponent,
         NavbarComponent,
         CraftComponent,
         CraftMapreduceComponent,
+        CraftStormComponent,
         DataAnalysisComponent,
         ToolboxComponent,
         DrawboardComponent,
@@ -53,6 +61,7 @@ import {SelectComponent} from "ng2-select";
 
         DatabaseControlComponent,
         ResultPresentationComponent,
+
     ],
     providers: [
         appRoutingProvider
