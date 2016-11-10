0e3b3fcd9960a2eb4b41d333abd8abc8e183f96a
ttaanngg <tangweiqiang@hotmail.com>
2016-11-10 15:03:25 +0800

    美化界面

--- src/app/navbar.component/navbar.component.ts
+++ src/app/navbar.component/navbar.component.ts
@@ -1,40 +1,41 @@
 import {Component, OnInit} from "@angular/core";
 import {NavbarService} from "../navbar.service";
 import {Router} from "@angular/router";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
-  // moduleId: module.id,
-  selector: 'app-navbar',
-  templateUrl: 'navbar.component.html',
-  styleUrls: ['navbar.component.css']
+    // moduleId: module.id,
+    selector: 'app-navbar',
+    templateUrl: 'navbar.component.html',
+    styleUrls: ['navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  private chartsList:{id:number,name:string}[] = [];
+    private chartsList: {id: number,name: string}[] = [];
 
-  constructor(private navbarService: NavbarService,private router: Router) {
-
-  }
-
-  get navbarTitle(): string {
-    return this.navbarService.title;
-  }
-  gotoChart(chart): void{
-    let link = ['/result-show', chart.id];
-    this.router.navigate(link);
-  }
-
-  ngOnInit() {
-    // 取消data-analys任务
-    this.chartsList = [
-      {id: 1, name: '柱形图'},
-      {id: 2, name: '饼图'}
-    ];
-    for(let chart of this.chartsList){
-      console.log(chart.id);
-      console.log(chart.name);
+    constructor(private navbarService: NavbarService, private router: Router, private sanitizer: DomSanitizer) {
     }
 
-  }
+    get navbarTitle(): string {
+        return this.navbarService.title;
+    }
+
+    gotoChart(chart): void {
+        let link = ['/result-show', chart.id];
+        this.router.navigate(link);
+    }
+
+    ngOnInit() {
+        // 取消data-analys任务
+        this.chartsList = [
+            {id: 1, name: '柱形图'},
+            {id: 2, name: '饼图'}
+        ];
+        for (let chart of this.chartsList) {
+            console.log(chart.id);
+            console.log(chart.name);
+        }
+
+    }
 
 }
