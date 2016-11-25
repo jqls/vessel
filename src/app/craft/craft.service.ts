910e60cf9aa0049be1e3fa510b8f81d8171f3ad6
Qiu Wenkai <chlwqwk@163.com>
2016-11-25 21:10:18 +0800

    feat(comonent): 完善toolbox的显示及数据获取

--- src/app/craft/craft.service.ts
+++ src/app/craft/craft.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import {WorkflowUnit} from "../share/data-types";
 
 @Injectable()
 export class CraftService {
+  private selectedNodeType: WorkflowUnit;
 
   constructor() { }
 
+  setSelectedNodeType(selectedNodeType: WorkflowUnit): void{
+    this.selectedNodeType = selectedNodeType;
+  }
+
+  getSelectedNodeType(): WorkflowUnit{
+    return this.selectedNodeType;
+  }
 }
--- src/app/craft/toolbox/toolbox.component.ts
+++ src/app/craft/toolbox/toolbox.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from "../../data.service";
+import {Dataset, WorkflowUnit} from "../../share/data-types";
+import {CraftService} from "../craft.service";
+import {mydebug} from "../../share/my-log";
 
 @Component({
   selector: 'app-toolbox',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbox.component.sass']
 })
 export class ToolboxComponent implements OnInit {
+  private debug_location: string = "ToolboxComponent";
+  private selectedNodeType: WorkflowUnit = null;
+  private datasets: Dataset[];
+  private algorithms: Algorithm[];
 
-  constructor() { }
+  constructor(private dataService: DataService,
+              private craftService: CraftService) {
+    this.dataService.getDatasets().then(datasets => {
+      this.datasets = datasets;
+    });
+    this.dataService.getAlgorithms().then(algorithms => {
+      this.algorithms = algorithms;
+    });
+  }
 
   ngOnInit() {
   }
 
+  itemClicked(item: WorkflowUnit) {
+    if (this.selectedNodeType == item) {
+      this.selectedNodeType = null;
+    } else {
+      this.selectedNodeType = item;
+    }
+    mydebug(this.debug_location,"itemClicked", this.selectedNodeType ? this.selectedNodeType.id : 'null');
+    this.craftService.setSelectedNodeType(this.selectedNodeType);
+  }
+
+  isHidden() {//下拉列表的收起和隐藏
+    var divid = document.getElementById("zhaoli");
+    $("#top1").click(function () {
+      var children = $(".second1");
+
+      if (children.is(":visible")) {
+        children.hide();
+        divid.className="triangle-collapsed icon-expand";
+      } else {
+        children.show()
+
+        divid.className="triangle-expanded icon-expand";
+      }
+    });
+  }
 }
