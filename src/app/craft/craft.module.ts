4e210bf84412701d7c766c912e1b7fe1ee42e6af
Qiu Wenkai <chlwqwk@163.com>
2016-11-25 19:26:09 +0800

    feat(service): init craft service
    用于提供Craft界面的相关服务

--- src/app/craft/craft.module.ts
+++ src/app/craft/craft.module.ts
@@ -4,12 +4,20 @@ import { CraftComponent } from './craft.component';
 import { CraftRoutingModule } from "./craft-routing.module";
 import { DrawboardComponent } from './drawboard/drawboard.component';
 import { ToolboxComponent } from './toolbox/toolbox.component';
+import {CraftService} from "./craft.service";
 
 @NgModule({
   imports: [
     CommonModule,
     CraftRoutingModule
   ],
-  declarations: [CraftComponent, DrawboardComponent, ToolboxComponent]
+  declarations: [
+    CraftComponent,
+    DrawboardComponent,
+    ToolboxComponent
+  ],
+  providers: [
+    CraftService
+  ]
 })
 export class CraftModule { }
