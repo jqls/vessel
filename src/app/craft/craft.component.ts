c2358e66b8381aedfae290397f013b42ec4d548e
Qiu Wenkai <chlwqwk@163.com>
2016-11-30 23:38:39 +0800

    fix(UI): 修改了Craft的边距和位置

--- src/app/craft/craft.component.ts
+++ src/app/craft/craft.component.ts
@@ -9,15 +9,25 @@ import {GlobalService} from "../global.service";
 })
 export class CraftComponent implements OnInit {
 
+  private isReload: boolean;
   constructor(private craftService:CraftService,
               private globalService:GlobalService) {
     this.globalService.setNavpaneStat(false);
+    this.isReload = this.craftService.isReload();
+    this.craftService.setReload(false);
   }
 
   ngOnInit() {
+    if(this.isReload)
+      this.reRender();
   }
   get isOpenRightPane() {return this.craftService.getRightPaneStat();}
+
   onSubmitClick() {
     this.craftService.submit();
+  }
+
+  reRender(){
+    //todo:需要跟朱博商量参数问题
   }
 }
