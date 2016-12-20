2563ffae864d36375bcbfe30e256adc8e6e7f9f2
auroral <1223490268@qq.com>
2016-12-20 19:08:25 +0800

    plotly实现可视化

--- src/app/share/data-show/bar/bar.component.ts
+++ src/app/share/data-show/bar/bar.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Input} from '@angular/core';
 import {DataJSON} from "../data-types";
 import * as echarts from "echarts";
 
+
 @Component({
   selector: 'app-bar',
   templateUrl: './bar.component.html',
@@ -10,10 +11,12 @@ import * as echarts from "echarts";
 export class BarComponent implements OnInit {
   @Input() data: Promise<DataJSON[]>;
   dataJSON:DataJSON[];
-  myChart;
-  constructor() {
-    this.myChart = echarts.init(document.getElementById('main') as HTMLDivElement);
-  }
+  /*
+   myChart;
+   constructor() {
+   this.myChart = echarts.init(document.getElementById('main') as HTMLDivElement);
+   }
+   */
   ngOnInit() {
     this.data.then((response: DataJSON[]) => {
       this.dataJSON = response;
@@ -24,51 +27,92 @@ export class BarComponent implements OnInit {
     console.error('An error occurred', error);
     return Promise.reject(error.message || error);
   }
+  /* echarts实现
+   private bar() {
+   return (json) => {
+   console.log("bar");
+   let option = {
+   tooltip: {
+   show: true
+   },
+   legend: {
+   data: ['数据平台']
+   },
+   xAxis: [
+   {
+   type: 'category',
+   data: (function () {
+   let data = [];
+   for (let i = 0; i < json.length; i++) {
+   console.log(json[i].NAME);
+   data.push(json[i].NAME);
+   }
+   return data;
+   })()
+   }
+   ],
+   yAxis: [
+   {
+   type: 'value'
+   }
+   ],
+   series: [
+   {
+   "name": "销量",
+   "type": "bar",
+   "data": (function () {
+   let data = [];
+   for (let i = 0; i < json.length; i++) {
+   console.log(json[i].VAL);
+   data.push(json[i].VAL);
+   }
+   return data;
+   })()
+   }
+   ]
+   };
+   this.myChart.setOption(option);
+   };
+   }
+   */
   private bar() {
-    return (json) => {
-      console.log("bar");
-      let option = {
-        tooltip: {
-          show: true
-        },
-        legend: {
-          data: ['数据平台']
-        },
-        xAxis: [
-          {
-            type: 'category',
-            data: (function () {
-              let data = [];
-              for (let i = 0; i < json.length; i++) {
-                console.log(json[i].NAME);
-                data.push(json[i].NAME);
-              }
-              return data;
-            })()
-          }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
-        series: [
-          {
-            "name": "销量",
-            "type": "bar",
-            "data": (function () {
-              let data = [];
-              for (let i = 0; i < json.length; i++) {
-                console.log(json[i].VAL);
-                data.push(json[i].VAL);
-              }
-              return data;
-            })()
-          }
-        ]
-      };
-      this.myChart.setOption(option);
-    };
-  }
+    var plotly = require('plotly.js/lib/core');
 
+    // Load in the trace types for bar
+    plotly.register([
+      require('plotly.js/lib/bar'),
+    ]);
+
+    module.exports = plotly;
+    return (json) => {
+
+      var data = [{
+        x: (()=> {
+          let data = [];
+          for (let key in json) {
+            data.push(
+              json[key].NAME
+            );
+          }
+          return data;
+        })(),
+        y: (()=> {
+          let data = [];
+          for (let key in json)
+            data.push(
+              json[key].VAL,
+            );
+          return data;
+        })(),
+        type: 'bar'
+      }];
+
+      var layout = {
+        height:480,
+        width: 680
+      };
+
+      plotly.newPlot(document.getElementById('main') as HTMLDivElement, data,layout);
+    }
+  }
 }
