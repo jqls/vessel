6774c3553b24f4d91282a1ba97b87c22bead00bb
Qiu Wenkai <chlwqwk@163.com>
2016-12-29 12:56:57 +0800

    fix(route): 修复Experiment界面下无法新建任务的Bug

--- src/app/drawer/drawer.component.ts
+++ src/app/drawer/drawer.component.ts
@@ -10,29 +10,35 @@ import {Router} from "@angular/router";
   styleUrls: ['./drawer.component.sass']
 })
 export class DrawerComponent implements OnInit {
-  private workflow_id:number;
+  private workflow_id: number;
   private debug_location = "DrawerComponent";
   private drawerActive: boolean;
   private selectedNode: WorkflowNode;
-  get isSelected(){
+
+  get isSelected() {
     this.selectedNode = this.globalService.getSelectedNode();
     return this.selectedNode != null;
   }
+
   constructor(private router: Router,
               private globalService: GlobalService) {
-    this.globalService.bookDrawerStat((active:boolean)=>{
-      if(active)
+    this.globalService.bookDrawerStat((active: boolean) => {
+      if (active)
         this.activeDrawer();
       else
         this.inactiveDrawer();
     });
     this.drawerActive = false;
     console.log(document.body.scrollHeight);
-    this.globalService.book_workflowID((id:number)=>{
+    this.globalService.book_workflowID((id: number) => {
       this.workflow_id = id;
     });
   }
-  get isVisualise() {return this.globalService.isVisual}
+
+  get isVisualise() {
+    return this.globalService.isVisual
+  }
+
   ngOnInit() {
   }
 
@@ -41,34 +47,42 @@ export class DrawerComponent implements OnInit {
     console.log(this.drawerActive);
     this.drawerActive = true;
     console.log(this.drawerActive);
-    d3.select(".drawer-inner").style("height",(document.documentElement.clientHeight-116)+"px");
+    d3.select(".drawer-inner").style("height", (document.documentElement.clientHeight - 116) + "px");
   }
+
   inactiveDrawer(): void {
     console.log("inactiveDrawer");
     console.log(this.drawerActive);
     this.drawerActive = false;
     console.log(this.drawerActive);
-    d3.select(".drawer-inner").style("height","60px");
+    d3.select(".drawer-inner").style("height", "60px");
   }
 
-  onClick(){
+  onClick(str: string) {
     this.globalService.setNavpaneStat(false);
     this.drawerActive = false;
-    d3.select(".drawer-inner").style("height","60px");
+    d3.select(".drawer-inner").style("height", "60px");
+    // if(str==='/Experiment')
+    this.router.navigateByUrl('').then(res => {
+      this.router.navigateByUrl(str);
+    });
+    // else
+    //   this.router.navigateByUrl(str);
   }
-  onRun(){
-    if(this.isVisualise)
+  onRun() {
+    if (this.isVisualise)
       return false;
     this.globalService.run();
   }
 
-  onSave(){
-    if(this.isVisualise)
+  onSave() {
+    if (this.isVisualise)
       return false;
     this.globalService.save();
   }
-  gotoRunHistory(){
-    if(this.isVisualise)
+
+  gotoRunHistory() {
+    if (this.isVisualise)
       return false;
     this.router.navigate(['/RunHistory']);
   }
