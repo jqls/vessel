0e8854b2e8ef0baef47b55556d8f64b4bf5be596
Qiu Wenkai <chlwqwk@163.com>
2017-05-20 13:41:29 +0800

    feat(environment): 添加自定义图表显示所需的URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,22 @@ export const environment = {
   production: true,
   isDebug: false,
   isMock: false,
-  URL_Spark: "http://10.5.0.222:8080/workflow/processor/0",
-  URL_Spark_mock: "app/spark_data",
-  URL_Spark_run: "http://10.5.0.222:8080/submit/",
-  URL_Spark_redraw: "http://10.5.0.222:8080/redraw?",
-  dataAnalysisServer: "10.5.0.224:5000",
-  djangoServer: "http://10.5.0.222:8080",
-  URL_Spark_log: "http://10.5.0.222:8080/processinformation?",
+  URL_Spark: 'http://10.5.0.222:8080/workflow/processor/0',
+  URL_Spark_mock: 'app/spark_data',
+  dataAnalysisServer: '10.5.0.224:5000',
+  djangoServer: 'http://10.5.0.222:8080',
+  URL_Spark_log: 'http://10.5.0.222:8080/processinformation?',
+
+  URL_Spark_redraw_mock: 'app/redraw',
+  URL_Spark_run: 'http://10.5.0.222:8080/dispatcher/submit_mission/',
+  URL_Spark_redraw: 'http://10.5.0.222:8080/workflow/workflow/?workflow_id=',
+  URL_Spark_RUN_HISTORY: 'http://10.5.0.222:8080/workflow/mission/0/?workflow_id=',
+  URL_Spark_save: 'http://10.5.0.222:8080/workflow/workflow/', // POST
+  URL_Spark_Workflow_History: 'http://10.5.0.222:8080/workflow/workflow/?workflow_id=0',
+  // 完整版GET http://10.5.0.222:8080/workflow/workflow/
+  URL_Spark_visualisation: 'http://10.5.0.222:8080/dispatcher/visualization/',
+  URL_Spark_processor_stat: 'http://10.5.0.222:8080/dispatcher/get_processor_status/', // 24-66
+  URL_Spark_SQL: 'http://10.5.0.222:8080/workflow/sql/',
+  // 新增自定义图表显示
+  URL_VISUAL_CUSTOME: 'http://10.5.0.222:8080/custom_visualization/', // workflowId-missionID-processorID-flowID-portID
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,17 +7,19 @@ export const environment = {
   production: false,
   isDebug: true,
   isMock: false,
-  URL_Spark: "http://10.5.0.222:8080/workflow/processor/0",
-  URL_Spark_mock: "app/spark_data",
-  URL_Spark_redraw_mock: "app/redraw",
-  URL_Spark_run: "http://10.5.0.222:8080/dispatcher/submit_mission/",
-  URL_Spark_redraw: "http://10.5.0.222:8080/workflow/workflow/?workflow_id=",
-  dataAnalysisServer: "10.5.0.224:5000",
-  URL_Spark_RUN_HISTORY: "http://10.5.0.222:8080/workflow/mission/0/?workflow_id=",
-  URL_Spark_save:"http://10.5.0.222:8080/workflow/workflow/",//POST
-  URL_Spark_Workflow_History:"http://10.5.0.222:8080/workflow/workflow/?workflow_id=0",
-  //完整版GET http://10.5.0.222:8080/workflow/workflow/
-  URL_Spark_visualisation: "http://10.5.0.222:8080/dispatcher/visualization/",
-  URL_Spark_processor_stat: "http://10.5.0.222:8080/dispatcher/get_processor_status/",//24-66
-  URL_Spark_SQL: "http://10.5.0.222:8080/workflow/sql/"
+  URL_Spark: 'http://10.5.0.222:8080/workflow/processor/0',
+  URL_Spark_mock: 'app/spark_data',
+  URL_Spark_redraw_mock: 'app/redraw',
+  URL_Spark_run: 'http://10.5.0.222:8080/dispatcher/submit_mission/',
+  URL_Spark_redraw: 'http://10.5.0.222:8080/workflow/workflow/?workflow_id=',
+  dataAnalysisServer: '10.5.0.224:5000',
+  URL_Spark_RUN_HISTORY: 'http://10.5.0.222:8080/workflow/mission/0/?workflow_id=',
+  URL_Spark_save: 'http://10.5.0.222:8080/workflow/workflow/', // POST
+  URL_Spark_Workflow_History: 'http://10.5.0.222:8080/workflow/workflow/?workflow_id=0',
+  // 完整版GET http://10.5.0.222:8080/workflow/workflow/
+  URL_Spark_visualisation: 'http://10.5.0.222:8080/dispatcher/visualization/',
+  URL_Spark_processor_stat: 'http://10.5.0.222:8080/dispatcher/get_processor_status/', // 24-66
+  URL_Spark_SQL: 'http://10.5.0.222:8080/workflow/sql/',
+  // 新增自定义图表显示
+  URL_VISUAL_CUSTOME: 'http://10.5.0.222:8080/custom_visualization/', // workflowId-missionID-processorID-flowID-portID
 };
