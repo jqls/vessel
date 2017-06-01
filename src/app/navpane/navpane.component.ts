cf23e7ad273add07b4e59876664d6b7159c405ef
Qiu Wenkai <chlwqwk@163.com>
2017-06-01 20:40:52 +0800

    feat(component): 折叠导航栏

--- src/app/navpane/navpane.component.ts
+++ src/app/navpane/navpane.component.ts
@@ -1,17 +1,20 @@
-import {Component, OnInit} from '@angular/core';
-import {GlobalService} from "../global.service";
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { GlobalService } from "../global.service";
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navpane',
   templateUrl: './navpane.component.html',
   styleUrls: ['./navpane.component.sass']
 })
-export class NavpaneComponent implements OnInit {
+export class NavpaneComponent implements OnInit, AfterViewInit {
 
   private isExpended: boolean;
+  private clicked = false;
 
-  constructor(private globalService:GlobalService) {
-    this.globalService.bookNavpaneStat((stat:boolean)=>{
+  constructor(private globalService: GlobalService) {
+    this.globalService.bookNavpaneStat((stat: boolean) => {
       this.isExpended = stat;
     });
     this.globalService.setNavpaneStat(true);
@@ -19,7 +22,19 @@ export class NavpaneComponent implements OnInit {
 
   ngOnInit() {
   }
-  onClick(){
+
+  ngAfterViewInit() {
+    const self = this;
+    $('#nav-data-analysis').click(() => {
+      if (!self.clicked) {
+        $('.second-nav').toggle('normal');
+      }else
+        self.clicked = false;
+    });
+  }
+
+  onClick() {
+    this.clicked = true;
     this.globalService.setNavpaneStat(true);
   }
 }
