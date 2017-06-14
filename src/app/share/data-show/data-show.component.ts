19860928e468e00ebcb4e22f48c7916790c8ae06
Qiu Wenkai <chlwqwk@163.com>
2017-06-14 21:19:39 +0800

    feat(component): 更换可视化

--- src/app/share/data-show/data-show.component.ts
+++ src/app/share/data-show/data-show.component.ts
@@ -3,6 +3,7 @@ import { DataJSON } from './data-types';
 import { DataShowService } from './data-show.service';
 import { handleError } from '../my-handler';
 import { environment } from "../../../environments/environment";
+import { Http } from "@angular/http";
 @Component({
   selector: 'app-data-show',
   templateUrl: './data-show.component.html',
@@ -22,41 +23,41 @@ export class DataShowComponent implements OnInit {
   formData = new FormData();
 
   // 获取任务列表zhaoli
-  constructor(private dataShowService: DataShowService) {
+  constructor(private dataShowService: DataShowService, private http: Http) {
     let info = dataShowService.getInfo();
     this.title = info.workflow_id + '-' + info.mission_id + '-' + info.processor_id + '-' + info.flow_id + '-' + info.port_id;
   }
 
   ngOnInit() {
-    if (this.visualization) {
-      this.dataJSON = this.dataShowService.requireData().then((response: String[]) => {
-        console.log(response);
-        let dataJSON = response.map(item => {
-          let str = item.split(',');
-          return {
-            NAME: str[0],
-            VAL: str[1]
-          };
-        });
-        this.datas = dataJSON;
-        console.log(dataJSON);
-        return dataJSON;
-      }).catch(handleError);
-    } else {
-      this.dataJSON = this.dataShowService.requireData().then((response: String[]) => {
-        console.log(response);
-        let dataJSON = response.map(item => {
-          let str = item.split(',');
-          return {
-            NAME: 'null',
-            table_value: str
-          };
-        });
-        this.datas = dataJSON;
-        console.log(dataJSON);
-        return dataJSON;
-      }).catch(handleError);
-    }
+    // if (this.visualization) {
+    //   this.dataJSON = this.dataShowService.requireData().then((response: String[]) => {
+    //     console.log(response);
+    //     let dataJSON = response.map(item => {
+    //       let str = item.split(',');
+    //       return {
+    //         NAME: str[0],
+    //         VAL: str[1]
+    //       };
+    //     });
+    //     this.datas = dataJSON;
+    //     console.log(dataJSON);
+    //     return dataJSON;
+    //   }).catch(handleError);
+    // } else {
+    //   this.dataJSON = this.dataShowService.requireData().then((response: String[]) => {
+    //     console.log(response);
+    //     let dataJSON = response.map(item => {
+    //       let str = item.split(',');
+    //       return {
+    //         NAME: 'null',
+    //         table_value: str
+    //       };
+    //     });
+    //     this.datas = dataJSON;
+    //     console.log(dataJSON);
+    //     return dataJSON;
+    //   }).catch(handleError);
+    // }
 
   }
 
@@ -64,6 +65,49 @@ export class DataShowComponent implements OnInit {
   setType(type: number) {
     this.type = type;
     console.log('show:' + this.type);
+    this.imgURL = null;
+    switch (type) {
+      case 5:
+        let URL1 = environment.URL_Spark_visualisation + this.title + '-1/';
+        this.http.get(URL1)
+          .toPromise()
+          .then(response => {
+              this.imgURL = (response.json())['picture'];
+              console.log('img url = ' + this.imgURL);
+            }
+          ).catch(handleError);
+        break;
+      case 6:
+        let URL2 = environment.URL_Spark_visualisation + this.title + '-2/';
+        this.http.get(URL2)
+          .toPromise()
+          .then(response => {
+              this.imgURL = (response.json())['picture'];
+              console.log('img url = ' + this.imgURL);
+            }
+          ).catch(handleError);
+        break;
+      case 7:
+        let URL3 = environment.URL_Spark_visualisation + this.title + '-3/';
+        this.http.get(URL3)
+          .toPromise()
+          .then(response => {
+              this.imgURL = (response.json())['picture'];
+              console.log('img url = ' + this.imgURL);
+            }
+          ).catch(handleError);
+        break;
+      case 8:
+        let URL4 = environment.URL_Spark_visualisation + this.title + '-4/';
+        this.http.get(URL4)
+          .toPromise()
+          .then(response => {
+              this.imgURL = (response.json())['picture'];
+              console.log('img url = ' + this.imgURL);
+            }
+          ).catch(handleError);
+        break;
+    }
   }
 
   modalShow() {// 控制拓扑图模态框的隐藏显示
